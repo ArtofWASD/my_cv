fda85816fa14662811f778d7898b16bd50dcaf5f
Leonid <Chunl3e666@yandex.ru>
2024-03-28 17:49:39 +0300

    delete config

--- next.config.mjs
+++ next.config.mjs
@@ -1,9 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    images: {
-        formats:['image/webp', 'image/avif'],
-        domains: ['skillicons.dev'],
-      }
+
 };
 
 export default nextConfig;
