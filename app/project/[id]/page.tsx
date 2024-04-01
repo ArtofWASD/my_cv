1f6314232aecbe049092a3703086ad021d2e98d9
Leonid <Chunl3e666@yandex.ru>
2024-04-01 15:11:27 +0300

    fix экспорт компонента

--- app/project/[id]/page.tsx
+++ app/project/[id]/page.tsx
@@ -1,9 +1,10 @@
 import { PageProps } from "@/src/shared/api/model/types"
 
-export const ProjectPage = async () => {
+const ProjectPage = async () => {
   return (
-    <div>
+    <>
       
-    </div>
+    </>
   )
 }
+ export default ProjectPage
