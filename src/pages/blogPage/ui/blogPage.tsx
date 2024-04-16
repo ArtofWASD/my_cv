979260a7c64bc7e1b681c342440b99d4d5979abf
Leonid <Chunl3e666@yandex.ru>
2024-04-16 16:31:44 +0300

    немного стилей

--- src/pages/blogPage/ui/blogPage.tsx
+++ src/pages/blogPage/ui/blogPage.tsx
@@ -5,10 +5,10 @@ const BlogPage = async () => {
   const supabase = createClient()
   const { data: posts } = await supabase.from("blog").select("*")
   return (
-    <div>
+    <div className="grid justify-center items-center">
       {posts &&
         posts.map((item) => (
-          <div key={item.id}>
+          <div key={item.id} className="my-2 py-4 border-2 rounded-2xl w-128">
             <h1>{item.title}</h1>
             <p>{item.body}</p>
             <p>{item.created_at.toLocaleString("ru-RU")}</p>
