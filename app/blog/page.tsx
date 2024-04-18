ad9449c0bec638ef4cd0180c84e883299531fe45
Leonid <Chunl3e666@yandex.ru>
2024-04-18 15:49:36 +0300

    fix: проблема стора

--- app/blog/page.tsx
+++ app/blog/page.tsx
@@ -1,9 +1,11 @@
 import BlogPage from "@/src/pages/blogPage/ui/blogPage";
 import { TypeData } from "@/src/shared/api/model/types";
-import { supabase } from "@/src/shared/api/supabase/handlerClient";
 import { Suspense } from "react";
-
+import { createClient } from "@/src/shared/api/supabase/supabaseServerClient";
+import { cookies } from "next/headers";
 const Blog = async () => {
+  const cookieStore = cookies();
+  const supabase = createClient(cookieStore);
   const { data: posts } = await supabase.from("blog").select("*");
   return (
     <main className="bg-gradient-to-b from-orange-200 to-blue-500">
