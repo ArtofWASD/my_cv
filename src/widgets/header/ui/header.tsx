19ed015d3e75b341ba813ed639a390a934990877
Leonid <Chunl3e666@yandex.ru>
2024-07-10 17:29:41 +0300

    Headers

--- src/widgets/header/ui/header.tsx
+++ src/widgets/header/ui/header.tsx
@@ -1,11 +1,9 @@
 "use client";
 import { usePathname } from "next/navigation";
 import Button from "@/src/shared/ui/button";
-import Toggler from "@/src/shared/ui/toggler";
 import { NavMenu } from "../../navMenu/ui/navMenu";
-import { useRouter } from "next/navigation"
+import { useRouter } from "next/navigation";
 import Image from "next/image";
-import { ThemeToggler } from "@/src/shared/ui/themeToggler";
 //TODO: добавить на кнопку настроек модальное окно если не авторизван пользователь и если авторизован то редиректить сразу в админку
 export const Header = () => {
   const pathname = usePathname();
@@ -17,28 +15,34 @@ export const Header = () => {
   ];
   return (
     <header className="grid bg-main-blue">
-      <div className="flex gap-2 justify-self-end pr-2 pt-2">
-        <ThemeToggler/>
+      <div className="flex gap-2 justify-self-end items-center pr-2 pt-2">
         <Image
-            src="/icons/settings.png"
-            width={40}
-            height={40}
-            alt="Picture of the author"
-            onClick={() => router.push("/admin")}
-            className="hover:scale-110 cursor-pointer"
-          />
+          src="/icons/settings.png"
+          width={40}
+          height={40}
+          alt="Picture of the author"
+          onClick={() => router.push("/admin")}
+          className="cursor-pointer hover:scale-110"
+        />
+        {pathname == "/" ? (
+          <></>
+        ) : (
+          <div className="none lg:block">
+            <Image
+              src="/icons/previous.png"
+              width={40}
+              height={40}
+              alt="Picture of the author"
+              onClick={() => router.push("/")}
+              className="cursor-pointer hover:scale-110"
+            />
+          </div>
+        )}
       </div>
       <div className="grid items-end justify-items-center lg:grid-cols-[95%_5%]">
         <div className="py-4 lg:ml-20">
           <NavMenu menuItems={menuItems} />
         </div>
-        {pathname == "/" ? (
-          <></>
-        ) : (
-          <div className="none self-center pt-4 lg:block">
-            <Button />
-          </div>
-        )}
       </div>
     </header>
   );
