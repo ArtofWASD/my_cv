a198bf2dd92393bedc0aebe7276453bdd034d3eb
Leonid <Chunl3e666@yandex.ru>
2024-07-12 14:02:59 +0300

    Возврат к базовому конфигу меню в шапке

--- src/widgets/navMenu/ui/navMenu.tsx
+++ src/widgets/navMenu/ui/navMenu.tsx
@@ -1,5 +1,4 @@
 "use client";
-
 import { motion } from "framer-motion";
 import Image from "next/image";
 import Link from "next/link";
@@ -12,49 +11,15 @@ interface INavItems {
 }
 
 export const NavMenu = ({ menuItems }: { menuItems: Array<INavItems> }) => {
-  const icon = {
-    initial: { opacity: 0, y: 0 },
-    hover: { scale: 1.2, y: -35, opacity: 1, transition: { duration: 0.5 } },
-    tap: { scale: 1, y: -30 },
-  };
-
-  const text = {
-    hover: { scale: 1.2 },
-    tap: { scale: 1 },
-  };
   return (
-    <>
-      <div className="flex justify-center gap-20">
-        {menuItems.map((item) => (
-          <motion.div
-            key={item.name}
-            initial="initial"
-            whileHover="hover"
-            whileTap="tap"
-          >
-            <Link
-              href={item.url}
-              className="grid items-end justify-items-center"
-            >
-              <motion.p
-                className="absolute cursor-pointer text-xl font-semibold text-white"
-                variants={text}
-              >
-                {item.name}
-              </motion.p>
-              <motion.div variants={icon}>
-                <Image
-                  src={item.img}
-                  width="45"
-                  height="45"
-                  alt={item.name}
-                  className="relative cursor-pointer"
-                />
-              </motion.div>
-            </Link>
-          </motion.div>
-        ))}
-      </div>
-    </>
+    <div className="grid grid-flow-col gap-5">
+      {menuItems.map((item) => (
+        <div key={item.name}>
+          <Link href={item.url} className="">
+            <p className="text-2xl font-semibold text-white">{item.name}</p>
+          </Link>
+        </div>
+      ))}
+    </div>
   );
 };
