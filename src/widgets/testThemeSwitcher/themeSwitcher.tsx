6c064e6072ff4d44126f036f1533ea15f6e32eea
Your Name <you@example.com>
2024-10-03 17:21:19 +0300

    Не получилось

--- src/widgets/testThemeSwitcher/themeSwitcher.tsx
+++ src/widgets/testThemeSwitcher/themeSwitcher.tsx
@@ -1,34 +1,39 @@
 import { useState, useEffect } from "react";
 import ThemeSwitcherIcon from "./themeSwitcherIcon";
 import { useTheme } from "next-themes";
-import { motion } from "framer-motion";
-import { MoonIcon } from "@heroicons/react/16/solid";
-import { init } from "next/dist/compiled/webpack/webpack";
+import { motion, useAnimate } from "framer-motion";
 
 //TODO: Подумать уменьшить икноки до 35px
 function ThemeSwitcher() {
   const [mounted, setMounted] = useState(false);
   const { setTheme, resolvedTheme } = useTheme();
   const [isOpen, setIsOpen] = useState(false);
+  const [scope, animate] = useAnimate()
 
-  const variants = {
-    sun: {
-      x: 7,
-      transition: { duration: 0.4 },
-    },
-    moon: {
-      x: -7,
-      transition: { duration: 0.4 },
-    },
-    moonBg: {
-      opacity: 1,
-      transition: { duration: 0.4 },
-    },
-    sunBg: {
-      opacity: 1,
-      transition: { duration: 0.4 },
-    },
-  };
+  // const moon = {
+  //   visible: {
+  //     x: 2,
+  //     opacity: 0,
+  //     transition: { duration: 0.4 },
+  //   },
+  //   hidden: {
+  //     x: -7,
+  //     opacity: 1,
+  //     transition: { duration: 0.4 },
+  //   },
+  // };
+  // const sun = {
+  //   visible: {
+  //     x: 7,
+  //     opacity: 1,
+  //     transition: { duration: 0.4 },
+  //   },
+  //   hidden: {
+  //     x: 14,
+  //     opacity: 0,
+  //     transition: { duration: 0.4 },
+  //   },
+  // };
 
   useEffect(() => {
     setMounted(true);
@@ -37,23 +42,25 @@ function ThemeSwitcher() {
   if (!mounted) return null;
   return (
     <motion.div
-      className={`rounded-full px-2 ${isOpen ? "bg-blue-500" : "bg-slate-900"}`}
-      animate={isOpen ? "moonBg" : "sunBg"}
-      variants={variants}
+      className={`rounded-full px-2  ${isOpen ? "bg-blue-500" : "bg-sky-900"}`}
     >
       <motion.button
+        ref={scope}
         onClick={() => {
           setTheme(resolvedTheme === "light" ? "dark" : "light"),
             setIsOpen((isOpen) => !isOpen);
         }}
-        className=" rounded-full py-1 mx-1"
-        animate={isOpen ? "sun" : "moon"}
-        variants={variants}
+        className=" rounded-full mx-1 py-1"
+
       >
         {resolvedTheme === "light" ? (
-          <ThemeSwitcherIcon theme="moon" />
+          <motion.div id="moon"  className="">
+            <ThemeSwitcherIcon theme="moon" />
+          </motion.div>
         ) : (
-          <ThemeSwitcherIcon theme="sun" />
+          <motion.div id="sun" className="">
+            <ThemeSwitcherIcon theme="sun" />
+          </motion.div>
         )}
       </motion.button>
     </motion.div>
