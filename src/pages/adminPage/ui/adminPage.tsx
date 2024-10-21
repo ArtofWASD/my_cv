bfa6d2ccf1514b44218d10ad965c1325ee90fa08
Your Name <you@example.com>
2024-10-21 17:34:19 +0300

    Доп правки

--- src/pages/adminPage/ui/adminPage.tsx
+++ src/pages/adminPage/ui/adminPage.tsx
@@ -6,12 +6,11 @@ import { AdminHeader } from "@/src/widgets/admin/adminHeader";
 import { AdminProjectsList } from "@/src/widgets/admin/adminProjectsList/ui";
 
 import { VerticalAdminMenu } from "@/src/widgets/admin/adminMenu/vertical";
-// Дизайн админки полное говно, надо переделать. под светлую и тёмную тему
-// А где блять цвет поменять ?!
+// Починить высоту меню, не достаётся до подвала. Разобраться с шириной мод мобилки.
 const AdminPage = () => {
   const status = useAdminStore((state: any) => state.adminStatus);
   return (
-    <div className="grid grid-cols-[10%_auto] grid-rows-[7%_auto]">
+    <div className="grid grid-cols-[100px minmax(0,1fr)] lg:grid-cols-[10%_auto] grid-rows-[7%_auto]">
       <div className="row-span-3">
         <VerticalAdminMenu />
       </div>
